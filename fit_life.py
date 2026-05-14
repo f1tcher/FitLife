66d4035d9bd7baf686b8f9c5007c8151215282de
Shabrin Denis <denis.shabrin73@mail.ru>
2026-05-14 15:19:53 +0300

    Перенёс константы в начало модуля, удалил лишнии комментарии, добил функцию replace()

--- fit_life.py
+++ fit_life.py
@@ -1,9 +1,14 @@
-# 1. Знакомство
-print('Здравствуйте, вы запустили тестовую версию FitLife')  # Приветствие
+"""Программа для расчёта ИМТ и Нормы воды в день"""
+
+WATER_PER_KG = 30  # Константа мл воды на кг
+ML_TO_L = 1000  # Константа миллилитров в литре
+DASHES_COUNT = 15  # Константа количества прочерков в отчёте
+
+print('Здравствуйте, вы запустили тестовую версию FitLife')
 
 
 def is_number(user_input):
-    """Создал функцию для проверки ввёл ли пользователь число"""
+    """Функция для проверки ввёл ли пользователь число"""
     try:
         user_input = float(user_input)
         return True
@@ -11,45 +16,37 @@ def is_number(user_input):
         return False
 
 
-# Запрашиваю имя пользователя и привожу к нижнему регистру
 user_name = input('Введите пожалуйста ваше имя: ').lower().title()
-user_age = input('А теперь возраст (числом): ')  # Запрашиваю возраст
-# И проверяю, ввёл ли пользователь число, иначе прошу ввести числом
+user_age = input('А теперь возраст (числом): ')
 while not is_number(user_age):
     user_age = input('Укажите пожалуйста возраст числом: ')
 user_age = int(user_age)
 
-# 2. Сбор данных
+
 user_weight = input('Введите пожалуйста ваш вес '
-                    '(в кг, числом в формате [0.00]): ')  # Запрашиваю вес
-while not is_number(user_weight):
+                    '(в кг, числом в формате [0.00]): ')
+while not is_number(user_weight.replace(',', '.')):
     user_weight = input('Укажите пожалуйста вес числом: ')
-user_weight = float(user_weight)
+user_weight = float(user_weight.replace(',', '.'))
 
 user_height = input('Теперь рост (в метрах, '
-                    'числом в формате [0.00]): ')  # Запрашиваю рост
-while not is_number(user_height):
+                    'числом в формате [0.00]): ')
+while not is_number(user_height.replace(',', '.')):
     user_height = input('Укажите пожалуйста рост числом: ')
-user_height = float(user_height)
+user_height = float(user_height.replace(',', '.'))
 
-# 3. Логика расчетов
-WATER_PER_KG = 30  # Константа мл воды на кг
-ML_TO_L = 1000  # Константа миллилитров в литре
 
-# Рассчитываю ИМТ и норму воды по формулам
 bmi = round(user_weight / (user_height ** 2), 1)
 water_ml = user_weight * WATER_PER_KG
 
-# Привожу мл в л для более понятного вывода
 water_l = round(water_ml / ML_TO_L, 1)
 
-# 4. Вывод красивого результата
-# Добавил проверку возраста, чтобы указать правильный суффикс
+
 age_suffix = ('г.' if (user_age % 10 in range(1, 5)
                        and not 11 <= user_age % 100 <= 14)
               else 'л.')
 
-print('-' * 15, 'Отчёт', '-' * 15)
+print('-' * DASHES_COUNT, 'Отчёт', '-' * DASHES_COUNT)
 print(f'Пользователь: {user_name} ({user_age} {age_suffix})')
 print(f'Индекс Массы Тела: {bmi}')
 print(f'Рекомендуемая норма воды: {water_l} л. в день')
